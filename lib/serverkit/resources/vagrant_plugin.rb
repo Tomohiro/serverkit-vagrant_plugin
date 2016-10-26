b688baaed1e5c051b6e3b6983fe0cd93a0e8e5da
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-10-26 13:32:58 +0900

    Implement check method

--- lib/serverkit/resources/vagrant_plugin.rb
+++ lib/serverkit/resources/vagrant_plugin.rb
@@ -9,6 +9,18 @@ module Serverkit
       def apply
         run_command("vagrant plugin install #{name}")
       end
+
+      # @note Override
+      def check
+        has_plugin?
+      end
+
+      private
+
+      # @return [true, false] True if the vagrant plugin is installed
+      def has_plugin?
+        check_command("vagrant plugin list | grep #{name}")
+      end
     end
   end
 end
